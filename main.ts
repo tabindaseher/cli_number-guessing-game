b65f3ccc590a1cf3fd551f18c8b2de5564faae93
tabindaseher <seherqureshi0602@gmail.com>
2024-06-07 04:26:57 -0700

    remove extra space

--- main.ts
+++ main.ts
@@ -8,7 +8,6 @@ import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
-
 const answer = await inquirer.prompt([{
     name : "userGuessNumber",
     type: "number",
